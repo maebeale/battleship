06385134a56c172aa849aa063d0519d2c87c5dbc
maebeale <maebeale@gmail.com>
2015-01-19 08:18:53 -0500

    pretty sure 11_cant_overlap_on_board is set, but now have very strange hangup.

--- board.rb
+++ board.rb
@@ -1,86 +1,44 @@
 require './ship'
+
 class Board
-  attr_accessor :ships, :squares_covered
+  attr_accessor :fleet, :squares_covered
 
   def initialize
-    @ships = []
+    @fleet = []
     @squares_covered = []
   end
 
   def has_ship_on?(x, y)
-    unless @ships == []
-      while @ships.any? { |s| s.covers?(x, y) }
+    unless @fleet == []
+      while @fleet.any? { |s| s.covers?(x, y) }
         return true
       end
-      return false
+      return false #TODO - both of these return falses needed?
     end
     return false
   end
 
   def place_ship(ship, x, y, across)
-    if ships.include?(ship)
-      puts "Can't place the ship twice"
-    elsif board_overlaps_with?(ship) == false
-      ship.place(x, y, across)
-      ship.squares.each { |ss|  @squares_covered << ss }
-     # @squares_covered << ship.squares
-      @ships << ship #TODO ok to end method w this?
-      puts "Ship was placed."
-    else
+    ship.projected_squares(x,y, across)
+    if ship.possible_squares.any? { |ss| self.has_ship_on?(ss[0],ss[1]) }
       raise "Can't overlap ships!"
-    end
-
-  end
-
-  def board_overlaps_with?(ship)
-    #if ship.squares.any? { |ss| self.has_ship_on?(ss[0],ss[1]) }
-    if ship.squares.any? { |ss| self.has_ship_on?(ss[0],ss[1]) }
-     return true
+      return  #TODO - is this needed here?
     else
-      return false
+      ship.place(x, y, across)
+      ship.squares.each { |ss| @squares_covered << ss }
+      @fleet << ship
+      return #TODO - is this needed here?
     end
-
-
-    # unless @ships == []
-    #   while @ships.any? { |s| s.overlaps_with?(ship) }
-    #     return true
-    #   end
-    #   return false
-    # end
-    # return false
   end
 
 end
-board = Board.new
-ship1 = Ship.new(4)
-ship2 = Ship.new(4)
-puts "ships..................: #{board.ships.length}"
-print "putting ship1 on board:" + " #{board.place_ship(ship1, 3, 3, true)} \n"
-print "putting ship2 on board:" + " #{board.place_ship(ship2, 3, 3, true)} \n"
-puts "ships..................: #{board.ships.length}"
-# p "board: #{board.squares_covered}"
-# # p "board ships: #{board.ships}"
-# p "ship1: #{ship2.squares}"
-# p "ship2: #{ship2.squares}"
+
+# board = Board.new
+# ship1 = Ship.new(4)
+# ship2 = Ship.new(4)
+# puts "0- #{board.fleet.length} ships on board, with covered squares = #{board.squares_covered} -0"
+# board.place_ship(ship1, 3, 3, true)
+# print "1- ship1 squares: #{ship1.squares} -1 \n"
+# puts "2- #{board.fleet.length} ships, with covered squares = #{board.squares_covered} -2"
+# puts "3- ships on board: #{board.fleet.inspect} -3"
 #
-# puts "ship1 covers 3,3: #{board.ships.first.covers?(3,3)}"
-# puts "ship2 covers 6,3: #{board.ships.second.covers?(6,3)}"
-# puts "board has ship on 2,3: #{board.has_ship_on?(2,3)}"
-# puts "board has ship on 3,3: #{board.has_ship_on?(3,3)}"
-# puts "board overlaps with ship1: #{board.ships.first.overlaps_with?(ship1)}"
-# puts "board overlaps with ship2: #{board.ships.first.overlaps_with?(ship2)}"
-# puts "#{board.place_ship(ship2, 1, 3, true)}"
-#
-# puts "number of ships on board: #{board.ships.length}"
-# # puts "covers result: #{board.ships.first.covers?(1,1)}"
-# # puts board.ships.first.squares.include?([1,1])
-# # if board.has_ship_on?(1, 1)
-# #   puts "yes, has ship on 1,1"
-# # else
-# #   puts 'no'
-# # end
-# # puts "has ship on: #{    unless @ships != []
-# #                            @ships.any? { |s| s.covers?(x, y)}
-# #                            return true
-# #                          end
-# # return false}"
--- ship.rb
+++ ship.rb
@@ -1,48 +1,57 @@
 # include 'set'
 
 class Ship
-  attr_accessor :length, :squares #, :placement_count
+  attr_accessor :length, :possible_squares, :squares, :placement_count, :hit_count
 
   def initialize(length)
     @length = length
+    @possible_squares = []
     @squares = []
     @placement_count = 0
+    @hit_count = 0
   end
 
   # creates an array of squares the ship will occupy when placed
-  def squares_calc(x, y, across)
-    square_count = @length
+  def projected_squares(x, y, across)
+    count = 0
+    @possible_squares = []
     if across == true
-      while square_count > 0
-        [x, y]
-        @squares << [x, y]
+      while count < @length
+        @possible_squares << [x, y]
         x += 1
-        square_count -= 1
+        count += 1
       end
-    else #across = false
-      while square_count > 0
-        [x, y]
-        @squares << [x, y]
+    else #across == false
+      while count < @length
+        @possible_squares << [x, y]
         y += 1
-        square_count -= 1
+        count -= 1
       end
     end
-    @squares
   end
 
-  # places ship on board. boolean toggle for orientation: across == true
+  # must be called from within board for projected_squares to be squares_covered.
+  # 3rd argument is a boolean toggle for orientation. default: across == true
   def place(x, y, across)
-    if @squares.include?([x,y]) || @placement_count == 1
-      raise "can't place this ship"
-    else
+    if @placement_count == 0 #&& !@squares.include?([x,y])
+      projected_squares(x, y, across)
+      @possible_squares.each { |ps| @squares << ps }
       @placement_count += 1
-      squares_calc(x, y, across)
     end
+    # else  #@squares.include?([x,y]) || @placement_count == 1
+    #   # raise "Can't place this ship again"
+    #   # return false#TODO problem is either here
+    #   # return #false
+    # end
+    # return false#TODO or here...
   end
 
   # assesses if ship is on a specific square
   def covers?(x, y)
-    if @squares.include?([x,y])
+    puts "#{ship.inspect}"
+    if @squares == []
+      return false
+    elsif @squares.include?([x,y])
       return true
     else
       return false
@@ -51,21 +60,52 @@ class Ship
 
   def overlaps_with?(other_ship)
     # compare the following two arrays:
-    other_ship.squares.any? { |ss| self.covers?(ss[0],ss[1]) }
+    other_ship.projected_squares.any? { |ss| self.covers?(ss[0],ss[1]) }
   end
 
   def fire_at(x, y)
     if covers?(x,y)
       @squares.delete([x,y])
+      hit_count += 1
     end
-
   end
 
   def sunk?
-    if squares == []
+    if @squares == [] || @hit_count == length #TODO - only need one of these
       return true
+    else
+      return false
     end
-    return false
   end
 
 end
+
+# test 3
+ship = Ship.new(4)
+puts "#{ship.inspect}"
+ship.place(2, 2, false)
+puts "#{ship.inspect}"
+ship.covers?(2, 1)
+ship.covers?(2, 2)
+ship.covers?(2, 3)
+ship.covers?(2, 4)
+ship.covers?(2, 5)
+ship.covers?(2, 6)
+ship.covers?(3, 2)
+
+
+# test 4
+# ship = Ship.new(4)
+# puts "#{ship.inspect}"
+# ship.place(2, 1, true)
+# puts "#{ship.inspect}"
+# ship.place(3, 2, false)
+# puts "#{ship.inspect}"
+
+# general checks
+# ship1 = Ship.new(4)
+# ship2 = Ship.new(4)
+# puts "0- #{ship1.inspect} -0"
+# puts "1- ship1.squares: #{ship1.length} -1"
+# print "2- #{ship1.place(3, 3, true)} -2 \n"
+# puts "3- #{ship1.inspect} -3"
